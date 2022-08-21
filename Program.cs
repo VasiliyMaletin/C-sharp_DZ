b924b529daaf0e4ad21f4eb0fb48d43fa9b9c0ee
Vasiliy Maletin <maletinvasilii@mail.ru>
2022-08-21 14:40:06 +0800

    task completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Task_2
+// Console.WriteLine("Введите число a и b: ");
+// Console.Write("a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if ( a > b )
+// {
+// Console.WriteLine("max = "+a+", min = "+b);
+// }
+// else
+// {
+// Console.WriteLine("max = "+b+", min = "+a);
+// }
+
+//Task_4
+// Console.WriteLine("Введите три числа: ");
+// Console.Write("Первое: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Второе: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Третье: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// while (b > max)
+//     {
+//     max = b;
+//     }
+// while (c > max)
+//     {
+//     max = c;
+//     } 
+// Console.Write("max = "+max);  
+
+//Task_6
+//  Console.Write("Введите целое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+//     {
+//     Console.WriteLine("да");
+//     }
+// else
+//     {
+//     Console.WriteLine("нет");
+//     }
+
+//Task_8
+// Console.Write("введите положительное целое число: ");
+// int a = Convert.ToInt32( Console.ReadLine());
+// int b = 2;
+// while (b <= a)
+//     {
+//     Console.Write(b+" ");
+//     b = b + 2;
+//     }
+// if (a <= 1)
+//     {
+//     Console.WriteLine("Введите число больше 1");
+//     }
